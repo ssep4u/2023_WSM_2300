31088d636aa1e7ed3d62a08a4c58d3764cd3f38f
SSep <ssep4u@gmail.com>
2023-06-15 11:17:01 +0900

    nav js 햄버거메뉴 작동 OK

---
+++ 20230601_nav/js/nav.js
@@ -0,0 +1,48 @@
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
+const navListUl = document.getElementsByClassName("nav-list")[0];
+
+console.log(navToggleI);
+console.log(navListUl);
+// .nav-toggle 클릭하면
+nav_toggle.onclick = () => {
+    // .nav-toggle i 햄버거메뉴 <-> X
+    // class에서 bi-list <-> bi-x-lg
+    navToggleI.classList.toggle("bi-list");
+    navToggleI.classList.toggle("bi-x-lg");
+
+    // .nav-list에 .show-menu toggle
+    navListUl.classList.toggle("show-menu");
+}
+
+
+//HTML -> js
+// 1. id
+//   const navToggleDiv = document.getElementById("nav-toggle");
+//   nav-toggle
+// 2. class
+//   const navToggleDiv = document.getElementsByClassName("nav-toggle")[0];
+// 3. Tag
+//   const navToggleDiv = document.getElementsByTagName("div")[0];
+// 4. CSS selector
+//   const navToggleDiv = document.querySelector("#nav-toggle");
+//   const navToggleDiv = document.querySelectorAll(".nav-toggle")[0];
+//   const navToggleDiv = document.querySelectorAll("div")[0];
+// 5. XPath
+// nav_toggle.onclick = () => alert("안녕");
+// nav_toggle.onclick = alert("안녕");
+
+//"안녕"이라는 창을 띄우는 함수를 실행해라
+// function sayHello() {
+//     alert("안녕");
+// }
+// nav_toggle.onclick = sayHello;
+
+// nav_toggle.onclick = function () {
+//     alert("안녕");
+// };
+
+// nav_toggle.onclick = () => {
+//     alert("안녕");
+// };
+
+// nav_toggle.onclick = () => alert("안녕");
